63ec5951d09aa64b6f6c5678f292462701aae007
ashish-kansagara <ashishkanashagara123@gmail.com>
2026-02-10 23:53:33 +0530

    data ingestion

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-from src.wineQT.config import configuration
+from wineQT import logger
+from wineQT.pipeline.state1_data_injetion import DataIngestionTrainingPipeline
+
+STAGE_NAME="Data ingestion stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
